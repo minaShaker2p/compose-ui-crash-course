f30f6100d013e576a48dd5672f08990500e2f91a
Mina Rezkalla <minashakeralfy@gmail.com>
2023-12-15 18:33:15 +0100

    list view in compose UI

--- app/src/main/java/com/example/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/composecrashcourse/MainActivity.kt
@@ -1,70 +1,111 @@
 package com.example.composecrashcourse
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecrashcourse.ui.theme.ComposeCrashCourseTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import kotlin.math.log
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCrashCourseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                var name by remember {
+                    mutableStateOf("")
                 }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        OutlinedTextField(
+                            value = name, onValueChange = { text ->
+                                name = text
+                            }, modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if (name.isNotBlank()) {
+                                names = names + name
+                                name = ""
+                            }
+                        }) {
+                            Text(text = "Add", color = Color.Black)
+                        }
+                    }
+                    namesList(names = names)
+
+                }
+
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    LazyColumn(Modifier.fillMaxSize()) {
-        items(count = 10)
-        { i ->
-            Icon(
-                imageVector = Icons.Default.Add, contentDescription = null,
-                modifier = Modifier.size(100.dp)
-            )
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        ComposeCrashCourseTheme {
+
         }
     }
 
-}
+    @Composable
+    fun namesList(names: List<String>) {
+        LazyColumn {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeCrashCourseTheme {
-        Greeting("Android")
+            items(names) { currentName ->
+                Text(
+                    text = currentName,
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+                Divider()
+
+            }
+        }
+
     }
 }
