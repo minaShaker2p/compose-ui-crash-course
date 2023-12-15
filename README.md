771dc94add7845024b94623bd9eb56b65ffb778f
Mina Rezkalla <minashakeralfy@gmail.com>
2023-12-15 11:02:44 +0100

    update Readme file

--- README.md
+++ README.md
@@ -3,3 +3,24 @@ This project is a crash course of compose UI , how to build views using compose 
 
 # Compose UI 
  Jetpack Compose UI it is a new way of building ui views in  android development.in compose ui we use Kotlin to build UI instead of using XML . 
+ compose UI is called Declarative UI that use states, it is a new way of thinking.
+
+# Compose : how it works ?
+composable function run sequentially.
+
+# Basic compose designs:
+ ### Two text views above each other 
+```kotlin
+Column {
+ Text(
+  text = "Hello World",
+  color = Color.Red,
+  fontSize = 30.sp
+ )
+ Text(
+  text = "Hello Mina",
+  color = Color.Blue,
+  fontSize = 30.sp
+ )
+}
+```
--- app/src/main/java/com/example/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/composecrashcourse/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.composecrashcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecrashcourse.ui.theme.ComposeCrashCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello World",
+            color = Color.Red,
+            fontSize = 30.sp
+        )
+        Text(
+            text = "Hello Mina",
+            color = Color.Blue,
+            fontSize = 30.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
