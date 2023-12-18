122f5cd91931b6ef1f302a6b0e422adf2e00b815
Mina Rezkalla <minashakeralfy@gmail.com>
2023-12-18 15:52:23 +0100

    Add Dark Mode

--- app/src/main/java/com/example/composecrashcourse/ListActivity.kt
+++ app/src/main/java/com/example/composecrashcourse/ListActivity.kt
@@ -1,8 +1,12 @@
 package com.example.composecrashcourse
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,22 +15,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composecrashcourse.ui.theme.ComposeCrashCourseTheme
+
 
 class ListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,29 +56,43 @@ class ListActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier, name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    //val extraPadding =   if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+
     Surface(
-        color = MaterialTheme.colorScheme.primary,
+        color = MaterialTheme.colorScheme.surface,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(24.dp), ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(bottom = extraPadding)
             ) {
-                Text(text = "Hello")
-                Text(text = "$name!")
+                Text(text = "Hello,")
+                Text(
+                    text = "$name", style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(imageVector = Icons.Filled.KeyboardArrowDown, contentDescription = null)
 
-                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
 
     }
 }
+
 
 @Composable
 private fun MyApp(
@@ -129,7 +152,10 @@ fun OnboardingPreview() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Composable
 fun GreetingPreview() {
     ComposeCrashCourseTheme {
